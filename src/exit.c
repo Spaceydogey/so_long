e4719739f4894854b3a03abb44c8d220e3a7aee8
Hugo Delmas <hdelmas@fu-r1-p7.s19.be>
2022-11-24 17:34:23 +0100

    dobonus

--- src/exit.c
+++ src/exit.c
@@ -6,7 +6,7 @@
 /*   By: hdelmas <hdelmas@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 10:51:08 by hdelmas           #+#    #+#             */
-/*   Updated: 2022/11/24 11:45:38 by hdelmas          ###   ########.fr       */
+/*   Updated: 2022/11/24 17:24:53 by hdelmas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ int	ft_exit_failure(t_arg *arg)
 {
 	destroy_image(arg->mlx, arg->sprites);
 	mlx_destroy_image(arg->mlx, arg->frame->img);
+	ft_putstr_fd("Error\n", 1);
 	exit(EXIT_FAILURE);
 	return (0);
 }
--- src/path_finding.c
+++ src/path_finding.c
@@ -6,43 +6,12 @@
 /*   By: hdelmas <hdelmas@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 17:17:50 by hdelmas           #+#    #+#             */
-/*   Updated: 2022/11/24 16:30:12 by hdelmas          ###   ########.fr       */
+/*   Updated: 2022/11/24 17:34:01 by hdelmas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-#include <stdio.h>
-/*
-void	print_map(t_map *map)
-{
-	int 		i = 0;
-	int			j;
-	char		c;
-
-	while (i < map->nbr_line)
-	{
-		j = 0;
-		while (j < map->len_line)
-		{
-			c = map->map[i][j];
-			if (c == PLAYER)
-				printf("\033[0;34m%c",c);//temp test
-			else if (c == '9')
-				printf("\033[0;31m%c",c);//temp test
-			else if (c == OBJ)
-				printf("\033[0;32m%c",c);//temp test
-			else if (c == EXIT)
-				printf("\033[0;33m%c",c);//temp test
-			else
-				printf("\033[0m%c",c);//temp test
-			j++;
-		}
-		printf("\n");//temp test
-		i++;
-	}
-}
-*/
 static int	is_obj_or_exit(char c, t_map *map)
 {
 	if (c == OBJ)
@@ -53,6 +22,11 @@ static int	is_obj_or_exit(char c, t_map *map)
 		return (1);
 	return (0);
 }
+static void	rec(int x, int y, t_map *map)
+{
+	path_finding(x, y, map);
+	map->map[y][x] = '9';
+}
 
 static int	pf_check(int x, int y, t_map *map)
 {	
@@ -60,43 +34,25 @@ static int	pf_check(int x, int y, t_map *map)
 	char	down;
 	char	left;
 	char	right;
-//	system("clear");
-//	usleep(10000);
+
 	map->map[y][x] = '9';
 	up = map->map[y - 1][x];
 	down = map->map[y + 1][x];
 	left = map->map[y][x - 1];
 	right = map->map[y][x + 1];
-//	print_map(map);
 	if (up == WALL && down == WALL && left == WALL && right == WALL)
 		return (-1);
-//	printf("up :%c\n",up);
 	if (up != WALL && up != '9')
-	{
-		path_finding(x, y - 1, map);
-		map->map[y - 1][x] = '9';
-	}
+		rec(x, y - 1, map);
 	right = map->map[y][x + 1];
-//	printf("right :%c\n",right);
 	if (right != WALL && right != '9')
-	{
-		path_finding(x + 1, y, map);
-		map->map[y][x + 1] = '9';
-	}
+		rec(x + 1, y, map);
 	left = map->map[y][x - 1];
-//	printf("lft :%c\n",left);
 	if (left != WALL && left != '9')
-	{
-		path_finding(x - 1, y, map);
-		map->map[y][x - 1] = '9';
-	}
+		rec(x - 1, y, map);
 	down = map->map[y + 1][x];
-//	printf("down :%c\n",down);
 	if (down != WALL && down != '9')
-	{
-		path_finding(x, y + 1, map);
-		map->map[y + 1][x] = '9';
-	}
+		rec(x, y + 1, map);
 	return (1);
 }
 
